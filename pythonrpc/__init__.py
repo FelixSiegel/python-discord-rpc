bbb7d19dc094c596ff6ee7e70a8e688417c11926
Felix Siegel <infinitycoding@web.de>
2024-09-10 18:20:14 +0200

    refactor: Update type_check_mypy.yml and ruff.toml
    - Rename the workflow name from "Type Check (Mypy)" to "Static Code Analysis" in type_check_mypy.yml.
    - Add ruff linting and formatting steps to type_check_mypy.yml.
    - Create ruff.toml configuration file with exclusion list and linting/formatting settings.

--- pythonrpc/__init__.py
+++ pythonrpc/__init__.py
@@ -4,9 +4,8 @@ A fully typed discord rich presence wrapper written in python.
 Author: Infinity Coding
 """
 
-
-__title__ = 'pythonrpc'
-__author__ = 'InfinityCoding'
-__version__ = '0.0.1'
-__copyright__ = 'Copyright (c) 2024 InfinityCoding'
-__license__ = 'MIT'
+__title__ = "pythonrpc"
+__author__ = "InfinityCoding"
+__version__ = "0.0.1"
+__copyright__ = "Copyright (c) 2024 InfinityCoding"
+__license__ = "MIT"
